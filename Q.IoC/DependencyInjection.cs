892faccef2a0033db352fa5c38ab34c9c9f44382
HodaMedhat <hoda.medhat.62@gmail.com>
2025-06-28 19:10:36 +0300

    extra/28/6/2025

---
+++ Q.IoC/DependencyInjection.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Q.Reporsitory.Mapping;
+
+
+namespace Q.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services,IConfiguration configuration)
+        {
+            //services.AddScoped<IGenericRepo<DistrictVM>, DistrictRepo>();
+            //services.AddScoped<IGenericService<DistrictVM>, DistrictService>();
+
+            //Auto Mapper Configurations
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
+
+            IMapper mapper = mapperConfig.CreateMapper();
+            services.AddSingleton(mapper);
+
+            return services;
+        }
+    }
+}
---
+++ Q.Reporsitory/Mapping/MappingProfile.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Q.EF;
+using Q.EF.Models;
+using Q.VM;
+using Q.VM.ViewModels;
+
+namespace Q.Reporsitory.Mapping
+{
+    public class MappingProfile :Profile
+    {
+        public MappingProfile() { 
+            CreateMap<BookMaster, BookMasterVM>().ReverseMap();
+            CreateMap<BooksDetail, BooksDetailVM>().ReverseMap();
+            CreateMap<BorrowingBook, BorrowingBookVM>().ReverseMap();
+            CreateMap<LkAuthor, LkAuthorVM>().ReverseMap();
+            CreateMap<LkBookStatus, LkBookStatusVM>().ReverseMap();
+            CreateMap<LkFormat, LkFormatVM>().ReverseMap();
+            CreateMap<LkPubilsher, LkPubilsherVM>().ReverseMap();
+            CreateMap<LkTrasnslator, LkTrasnslatorVM>().ReverseMap();
+            CreateMap<LkWarnng, LkWarnngVM>().ReverseMap();
+            CreateMap<PurchseingBook, PurchseingBookVM>().ReverseMap();
+            CreateMap<Read, ReadVM>().ReverseMap();
+            CreateMap<UserLibrary, UserLibraryVM>().ReverseMap();
+            CreateMap<User, UserVM>().ReverseMap();
+            CreateMap<Warning, WarningVM>().ReverseMap();
+            
+            
+          
+           
+            
+            
+            
+            
+            
+        }
+    }
+}
